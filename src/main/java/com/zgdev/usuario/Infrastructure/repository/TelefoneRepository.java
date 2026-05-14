762acc847b2ed9f6a246c087054413fa08b29a7a
Rafazg <claudenirorafaelbjj@gmail.com>
2026-05-14 06:05:11 -0300

    Adicionadas classes de security

---
+++ src/main/java/com/zgdev/usuario/Infrastructure/repository/TelefoneRepository.java
@@ -0,0 +1,10 @@
+package com.zgdev.usuario.Infrastructure.repository;
+
+import com.zgdev.usuario.Infrastructure.entity.Telefone;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface TelefoneRepository extends JpaRepository<Telefone, Long>{
+
+}
