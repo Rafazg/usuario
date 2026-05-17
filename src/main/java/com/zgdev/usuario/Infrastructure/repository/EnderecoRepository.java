1781a2d427c48881fdda20795d6db6ec18d8370f
Rafaelsilva <99143713+Rafazg@users.noreply.github.com>
2026-05-17 10:30:32 -0300

    Revert "Feature/cadastro endereco telefone"

--- src/main/java/com/zgdev/usuario/Infrastructure/repository/EnderecoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.zgdev.usuario.Infrastructure.repository;
-
-import com.zgdev.usuario.Infrastructure.entity.Endereco;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EnderecoRepository extends JpaRepository<Endereco, Long>{
-
-}
