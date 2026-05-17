d2fadc3cd0ea9134f93d20debe6c74998d88efb6
Rafaelsilva <99143713+Rafazg@users.noreply.github.com>
2026-05-17 10:50:13 -0300

    Revert "Developer"

---
+++ src/main/java/com/zgdev/usuario/Infrastructure/entity/Telefone.java
@@ -0,0 +1,28 @@
+package com.zgdev.usuario.Infrastructure.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "telefone")
+@Builder
+public class Telefone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "numero", length = 11)
+    private String numero;
+
+    @Column(name = "ddd", length = 2)
+    private String ddd;
+
+    @Column(name = "usuario_id")
+    private Long usuario_id;
+}
---
+++ src/main/java/com/zgdev/usuario/Infrastructure/repository/EnderecoRepository.java
@@ -0,0 +1,8 @@
+package com.zgdev.usuario.Infrastructure.repository;
+
+import com.zgdev.usuario.Infrastructure.entity.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EnderecoRepository extends JpaRepository<Endereco, Long>{
+
+}
